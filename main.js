5ab853e4fc2051a17cfb88909661f17b55b88f1d
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-07-03 15:37:39 -0400

    all

---
+++ main.js
@@ -0,0 +1,42 @@
+const para = document.querySelector("p");
+
+para.style.color = 'lightblue';
+
+const headChange = document.querySelector("h1");
+
+headChange.style.fontSize = "48px";
+
+const pa = document.querySelector("p");
+
+pa.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+const halfTrans = document.querySelector("#item-13");
+
+halfTrans.style.opacity = "0.5";
+
+
+const insertWord = document.querySelector("#item-3");
+
+insertWord.innerText = "HI!";
+
+const imgChange = document.querySelector("img");
+
+imgChange.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg";
+
+imgChange.width = "300";
+
+//const imgChange2 = document.querySelector("asmr.png");
+
+//imgChange2.src = "asmr.png";
+
+const imgChange2 = document.querySelector("#picture-1");
+
+imgChange2.src = "/Users/readushartoniii/Desktop/Workspace/this-might-be-a-domb-idea/asmr.png";
+
+imgChange2.width = "300";//does not show change
+
+const newLi = document.querySelector("#item-16");
+
+
+
+newLi.innerText = "Won't get fooled again.";
